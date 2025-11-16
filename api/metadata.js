bf3af7b8db67a55bc7556b9ccced6ba1b270e676
sreeman37 <sreeman.potlapalli@gmail.com>
2025-11-16 16:24:48 +0530

    Delete api/metadata.js

--- api/metadata.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-    const user = req.query.user_id;
-
-    const verified = globalThis.users?.[user]?.verified ?? 0;
-
-    res.json({
-        metadata: { verified }
-    });
-};
