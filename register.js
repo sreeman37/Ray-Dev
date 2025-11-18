62f4f57f02f49055d4408fb6a276ee95393591e5
sreeman37 <sreeman.potlapalli@gmail.com>
2025-11-18 12:02:33 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,11 +1,15 @@
-import fetch from "node-fetch";
-
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
 const commands = [
-  { name: "ping", description: "Show real-time ping" },
-  { name: "stats", description: "Show system stats" }
+  {
+    name: "ping",
+    description: "Show real-time ping"
+  },
+  {
+    name: "stats",
+    description: "Show system stats"
+  }
 ];
 
 await fetch(`https://discord.com/api/v10/applications/${CLIENT_ID}/commands`, {
@@ -17,4 +21,4 @@ await fetch(`https://discord.com/api/v10/applications/${CLIENT_ID}/commands`, {
   body: JSON.stringify(commands),
 });
 
-console.log("Slash commands registered");
+console.log("Updated all commands!");
