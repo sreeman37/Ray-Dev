37b98c47c7c5aa126d3d2dabe49272baf7d920d8
sreeman37 <sreeman.potlapalli@gmail.com>
2025-11-22 03:03:46 +0530

    Update discord.js

--- api/discord.js
+++ api/discord.js
@@ -1,34 +1,19 @@
 import { InteractionType, InteractionResponseType, verifyKeyMiddleware } from "discord-interactions";
 
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Allowed');
+export default function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).send("Method Not Allowed");
     return;
   }
 
-  verifyKeyMiddleware(process.env.PUBLIC_KEY)(req, res, async () => {
+  verifyKeyMiddleware(process.env.PUBLIC_KEY)(req, res, () => {
     const interaction = req.body;
 
     if (interaction.type === InteractionType.PING) {
-      return res.status(200).json({ type: InteractionResponseType.PONG });
+      res.status(200).json({ type: InteractionResponseType.PONG });
+      return;
     }
 
-    if (
-      interaction.type === InteractionType.APPLICATION_COMMAND &&
-      interaction.data?.name === "ping"
-    ) {
-      const timestamp = Date.now();
-      const latency = 0;
-      const apiLatency = 0;
-
-      return res.status(200).json({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Latency: \`${latency}ms\`\nAPI Latency: \`${apiLatency}ms\``,
-        },
-      });
-    }
-
-    return res.status(200).end();
+    res.status(200).end();
   });
 }
